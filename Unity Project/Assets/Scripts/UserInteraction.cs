ce722f2608a3c99e5536fb110d31c9007bf052fc
Alexander Crichton <alexjcrichton01@gmail.com>
2014-03-23 23:05:41 +1000

    Updated sequence diagram. Added Unity project.

---
+++ Unity Project/Assets/Scripts/UserInteraction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInteraction : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            userLeftClicked();
+        }
+    }
+
+    private void userLeftClicked()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Clicked on " + hit.collider.name);
+            Application.OpenURL("http://localhost");
+        }
+    }
+}
