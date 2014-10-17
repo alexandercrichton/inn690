086cd03a0e4e67fdcc8a13c4fe9951d0644b6504
Alexander Crichton <alexjcrichton01@gmail.com>
2014-10-17 20:19:14 +1000

    Big changes to gitignore. Some necessary files had been ignored.

---
+++ YAWL/YAWL Relay/src/main/ClientListenServer.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ClientListenServer implements Runnable {
+	
+	protected int _port;
+	
+	public ClientListenServer(int port) {
+		this._port = port;
+	}
+
+	public void run() {
+		try {
+			System.out.println("ClientListenServer listening on port " + _port);
+			ServerSocket serverSocket = null;
+			boolean listening = false;
+
+			try {
+				serverSocket = new ServerSocket(_port);
+				listening = true;
+			} catch (IOException e) {
+				System.err.println("ClientListenServer: Could not listen on port: " + _port);
+				System.err.println(e.getMessage());
+				return;
+			}
+
+			while (listening) {
+				Application.Executor.execute(new ClientHandler(serverSocket.accept()));
+			}
+			
+			serverSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
